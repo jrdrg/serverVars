76335980c4f021afe7647699cf0adff3873910db
Rob Richard <rob@1stdibs.com>
2017-01-31 18:21:28 -0500

    remove lodash.clone and find-namespace-value

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
 'use strict';
 var Mutable = require('./Mutable');
 var serverSideServerVars = require('./serverSide');
-var clone = require('lodash.clone');
+
 module.exports = function (req, res, next) {
     res.serverVars = res.locals.serverVars = new Mutable();
-    res.serverVars.add(clone(serverSideServerVars.get())); // clone the app-wide serverVars
+    res.serverVars.add(Object.assign({}, serverSideServerVars.get())); // clone the app-wide serverVars
     next();
 };
